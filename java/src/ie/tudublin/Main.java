fbcd7bfce5d1322e128212b37aead01bb966581a
Kelvin Oko <c17306113@mydit.ie>
2019-03-21 10:55:33 +0000

    a

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -10,9 +10,15 @@ public class Main
 		
 	}
 
+	public void starMap()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new StarMap());
+		
+	}
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.starMap();			
 	}
 }
