4d2f84021b1e13b512eab0e9d0e872ba5b36cff8
Kelvin Oko <c17306113@mydit.ie>
2019-03-27 18:37:08 +0000

    z

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -42,19 +42,19 @@ public class UI extends PApplet
     public void setup()
     {
         circle = new Circle(200, this);
-        radar = new Radar(this, 280, 280, 460);
+        radar = new Radar(this, 320, 320, 500);
         grid = new Grid(500, 150, 500, 150, 900, 550, 0, 0, this);
         sys1 = new SolarSystem(this);
 
-        planets.add(new Planet(280, 280, 100, 100, 255, 150, 0, 255, this));//sun
-        planets.add(new Planet(340, 280, 20, 20, 204, 102, 0, 255, this));//mercury
-        planets.add(new Planet(342, 240, 25, 25, 255, 102, 0, 255, this));//venus
-        planets.add(new Planet(208, 213, 40, 40, 0, 0, 190, 255, this));//earth
-        planets.add(new Planet(372, 360, 32, 32, 255, 51,0, 255, this));//mars
-        planets.add(new Planet(180, 390, 52, 52, 255, 204, 102, 255, this));//jupiter
-        planets.add(new Planet(430, 180, 43, 43, 255, 204, 102, 255, this));//
-        planets.add(new Planet(290, 130, 41, 41, 40, 204, 240, 255, this));
-        planets.add(new Planet(84, 260, 41, 41, 0, 102, 204, 255, this));
+        planets.add(new Planet(320, 320, 100, 100, 255, 150, 0, 255, this));//sun
+        planets.add(new Planet(380, 320, 20, 20, 204, 102, 0, 255, this));//mercury
+        planets.add(new Planet(382, 280, 25, 25, 255, 102, 0, 255, this));//venus
+        planets.add(new Planet(248, 253, 40, 40, 0, 0, 190, 255, this));//earth
+        planets.add(new Planet(412, 400, 32, 32, 255, 51,0, 255, this));//mars
+        planets.add(new Planet(220, 430, 52, 52, 255, 204, 102, 255, this));//jupiter
+        planets.add(new Planet(455, 225, 43, 43, 255, 204, 102, 255, this));//saturn
+        planets.add(new Planet(330, 170, 41, 41, 40, 204, 240, 255, this));//uranus
+        planets.add(new Planet(124, 300, 41, 41, 0, 102, 204, 255, this));//neptune
         
 
     }
