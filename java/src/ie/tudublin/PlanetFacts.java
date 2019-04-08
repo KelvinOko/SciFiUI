9cfe5e1480d3bb5c958b57bc7164fa909bf34f84
Kelvin Oko <c17306113@mydit.ie>
2019-04-08 15:31:46 +0100

    csv for 3d planets(incomplete)

---
+++ java/src/ie/tudublin/PlanetFacts.java
@@ -0,0 +1,70 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import processing.data.TableRow;
+
+public class PlanetFacts
+{
+    // private String name, age, facts;
+    // PApplet ui;
+
+    // public PlanetFacts(TableRow row)
+    // {
+    //     this.name = row.getString("Name:");
+    //     this.age = row.getString("Age:");
+    //     this.facts = row.getString("Interesting Facts:");
+    // }
+
+    // /**
+    //  * @return the name
+    //  */
+    // public String getName() 
+    // {
+    //     return name;
+    // }
+
+    // /**
+    //  * @param name the name to set
+    //  */
+    // public void setName(String name) 
+    // {
+    //     this.name = name;
+    // }
+
+    // /**
+    //  * @return the name
+    //  */
+    // public String getAge() 
+    // {
+    //     return Age;
+    // }
+
+    // /**
+    //  * @param name the name to set
+    //  */
+    // public void setAge(String Age) 
+    // {
+    //     this.Age = Age;
+    // }
+
+    // /**
+    //  * @return the name
+    //  */
+    // public String getFacts() 
+    // {
+    //     return Interesting Facts;
+    // }
+
+    // /**
+    //  * @param name the name to set
+    //  */
+    // public void setName(String name) 
+    // {
+    //     this.name = name;
+    // }
+
+    
+
+    
+
+}
